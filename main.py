4dd8ba74106c23c85141c107a294f43bb6301561
Nick Watson <>
2021-06-02 00:03:06 +0000

    Added problem #2

--- main.py
+++ main.py
@@ -18,3 +18,33 @@ class Solution:
 
 # ===============
 
+from collections import deque
+
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.arr = [None] * n
+        self.index = 0
+        
+
+    def insert(self, key: int, value: str) -> List[str]:
+        self.arr[key-1] = value
+        newArr = []
+        
+        if (key - 1) == self.index:
+            for i in range(self.index, len(self.arr)):
+                if self.arr[self.index] == None:
+                    break
+                else:
+                    newArr.append(self.arr[self.index])
+                    self.index += 1
+            
+            return newArr
+
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
+
+# ===============
+
