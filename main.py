d7d832f97ea158d50b9aaf24e2a1883b7c25391f
Nick Watson <>
2021-06-02 00:09:10 +0000

    Added problem #3

--- main.py
+++ main.py
@@ -48,3 +48,18 @@ class OrderedStream:
 
 # ===============
 
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        
+        word_1 = "".join(word1)
+        word_2 = "".join(word2)
+        
+        if word_1 == word_2:
+            return True
+        else:
+            return False
+        
+        # Method 2
+        # return "".join(word1) == "".join(word2)
+
+# ===============
